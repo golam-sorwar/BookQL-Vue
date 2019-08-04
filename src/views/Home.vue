797d6685f2df67d6a5aed28aa963a4bb60b57fb7
Golam Sorwar <akibmd719@gmail.com>
2019-08-04 21:39:16 +0600

    setup Vue Apollo and cors platform setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,52 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ApolloQuery :query="require('../graphql/queries/Categories.gql')">
+      <template slot-scope="{ result: { data, loading }, isLoading }">
+        <div v-if="isLoading">Loading...</div>
+        <ul v-else>
+          <a
+            href="#"
+            v-for="category of data.categories"
+            :key="category.id"
+            class="user"
+          >
+            {{ category.id }} {{ category.name }}
+          </a>
+        </ul>
+      </template>
+    </ApolloQuery>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import gql from "graphql-tag";
+// eslint-disable-next-line no-unused-vars
 
 export default {
   name: "home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      categories: []
+    };
+  },
+  apollo: {
+    // Simple query that will update the 'hello' vue property
+    categories: gql`
+      {
+        categories {
+          id
+          name
+        }
+      }
+    `
   }
 };
 </script>
+
+<style>
+.link-margin {
+  margin-left: 24px;
+}
+</style>
